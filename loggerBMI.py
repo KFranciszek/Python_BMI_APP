0217309a7acc1d45e0af472340eec6af7c90636c
KFranciszek <krzysztof.zernicki@gmail.com>
2017-08-21 21:36:20 +0200

    Update loggerBMI.py

--- loggerBMI.py
+++ loggerBMI.py
@@ -1,1 +1,14 @@
 # Usługa do zapisywania logów działania oraz błędów do pliku. 
+
+#logowanie wywołania usługi dicBMI
+import logging
+import dicBMI
+
+formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s'
+logging.basicConfig(filename='D:\Python\Projekt\python1.log',
+                    level=logging.DEBUG,
+                    format=formatLogging)
+logger=logging.getLogger()
+logger.info(dicBMI)
+
+print(logger.level)
