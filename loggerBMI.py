013218cb7ec352b37829bb9349440913595fb157
KFranciszek <krzysztof.zernicki@gmail.com>
2017-09-03 19:55:39 +0200

    Update loggerBMI.py

--- loggerBMI.py
+++ loggerBMI.py
@@ -5,7 +5,7 @@ import logging
 import dicBMI
 from dicBMI import team
 
-formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s'
+formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s']
 logging.basicConfig(filename='D:\Python\Projekt\python1.log',
                     level=logging.DEBUG,
                     format=formatLogging)
