7825168bef095f34e42063d73aa241cd181441ac
KFranciszek <krzysztof.zernicki@gmail.com>
2017-11-23 22:08:29 +0100

    Update dicBMI.py
    versja 2.0

--- dicBMI.py
+++ dicBMI.py
@@ -1,18 +1,20 @@
 team = []
+
 def DicBMI_data():
-    member= True
+    member = True
     while member:
+
         name = input('Podaj imie')
         weight = int(input('Podaj swoją wage'))
         height = float(input('Podaj swój wzrost'))
         bmi = weight//(2*height)
-        bmiDic = {'name': name,
-                  'bmi': bmi}
-
+        bmiDic = {'name': name, 'weight': weight, 'height':height,'bmi': bmi}
         team.append(bmiDic)
         pytanie = input('Czy policzyć jeszcze raz  T/N')
         if pytanie == 'N':
             member = False
-            return team and bmiDic
 
-DicBMI_data()
+    return  (team,member)
+
+
+BMI_data=DicBMI_data()
