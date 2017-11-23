546deeab9dd828e8c129f4016b7a388ebf6f2825
KFranciszek <krzysztof.zernicki@gmail.com>
2017-11-23 22:10:49 +0100

    Update loggerBMI.py

--- loggerBMI.py
+++ loggerBMI.py
@@ -3,13 +3,27 @@
 #logowanie wywołania usługi dicBMI
 import logging
 import dicBMI
-from dicBMI import team
+from dicBMI  import BMI_data
 
-formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s']
-logging.basicConfig(filename='D:\Python\Projekt\python1.log',
-                    level=logging.DEBUG,
-                    format=formatLogging)
-logger=logging.getLogger()
-logger.info(dicBMI)
-logger.info(team)
+def loggingDEBUG():
 
+    formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s'
+
+    logging.basicConfig(filename='D:\Python\Projekt\python1.log',
+                        level=logging.DEBUG,
+                        format=formatLogging)
+    logger = logging.getLogger()
+    logger.info(dicBMI)
+    logger.info(BMI_data)
+
+def loggingERROR ():
+    formatLogging = '[%(asctime).20s] [%(levelname)s] %(message)s'
+    logging.basicConfig(filename='D:\Python\Projekt\pythonERROR.log',
+                        level=logging.ERROR,
+                        format=formatLogging)
+    logger = logging.getLogger()
+    logger.error(dicBMI)
+    logger.error(BMI_data)
+
+loggingERROR()
+loggingDEBUG()
