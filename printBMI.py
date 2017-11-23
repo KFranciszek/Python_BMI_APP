7b25b9e56d0f4d5bcf2afde4fba405bda515e49f
KFranciszek <krzysztof.zernicki@gmail.com>
2017-11-23 22:11:45 +0100

    Update printBMI.py

--- printBMI.py
+++ printBMI.py
@@ -1,10 +1,15 @@
-from dicBMI import team
+import dicBMI
+import scoreBMI
+# from dicBMI  import BMI_data
+from dicBMI  import team
+# from dicBMI  import bmiDic
+# from scoreBMI  import scoreBMI_data
+# from scoreBMI  import score2
+# from scoreBMI  import score
 
 def BMI_print ():
+    for dict in team:
+     print(dict['name'],  " twoje BMI wynosi: ", dict['bmi'])
 
-
-    for i in team:
-        print(i)
-    return i
 
 BMI_print()
