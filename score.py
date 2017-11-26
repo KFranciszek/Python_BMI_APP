c91d93668119e495b4d0b4469066efba985da092
KFranciszek <krzysztof.zernicki@gmail.com>
2017-11-26 13:00:41 +0100

    Create score.py

---
+++ score.py
@@ -0,0 +1,10 @@
+from dicBMI  import team
+from scoreBMI import  data
+
+
+def BMI_print ():
+    for dict in team:
+     print(dict['name'],  " twoje BMI wynosi: ", dict['bmi'], "twój status BMI to: ",)
+
+
+BMI_print()
