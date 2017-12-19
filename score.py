7d597f52d5d3cde2bf6ef4f25f1217c6afa7a9c3
KFranciszek <krzysztof.zernicki@gmail.com>
2017-12-19 19:02:03 +0100

    Update score.py

--- score.py
+++ score.py
@@ -8,20 +8,4 @@ def scoreBMI_data (bmi):
 
     elif bmi >= 26:
         return "Nadwaga"
-    
 
-
-
-
-
-
-# from dicBMI  import team
-# from scoreBMI import  data
-
-
-# def BMI_print ():
-#     for dict in team:
-#      print(dict['name'],  " twoje BMI wynosi: ", dict['bmi'], "twój status BMI to: ",)
-
-
-# BMI_print()
