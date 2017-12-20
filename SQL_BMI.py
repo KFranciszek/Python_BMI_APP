a6da293ee9bcc6895dc8ad8f579e614c2092a6d9
KFranciszek <krzysztof.zernicki@gmail.com>
2017-12-20 21:28:40 +0100

    Update SQL_BMI.py

--- SQL_BMI.py
+++ SQL_BMI.py
@@ -7,7 +7,6 @@ try:
         print("Dobre połączenie do bazy postgres")
         cur = conn.cursor()
         cur.execute("""INSERT INTO bmi(name,weight,height,bmi) VALUES (%(name)s,%(weight)s, %(height)s,%(bmi)s)""", bmiDic)
-        # cur.execute("INSERT INTO public.bmi (name) VALUES  ((weight))", dict[bmiDic])
         conn.commit()
         cur.close()
 
