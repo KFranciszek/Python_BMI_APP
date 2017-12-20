ea9d75dd6759d4819817e9000190c9edb5329f29
KFranciszek <krzysztof.zernicki@gmail.com>
2017-12-20 21:28:14 +0100

    Update SQL_BMI.py

--- SQL_BMI.py
+++ SQL_BMI.py
@@ -3,7 +3,7 @@ from dicBMI  import team
 
 try:
     for bmiDic  in team:
-        conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgresq123'")
+        conn = psycopg2.connect("dbname='XYZ' user='XYZ' host='localhost' password='XYZ'")
         print("Dobre połączenie do bazy postgres")
         cur = conn.cursor()
         cur.execute("""INSERT INTO bmi(name,weight,height,bmi) VALUES (%(name)s,%(weight)s, %(height)s,%(bmi)s)""", bmiDic)
