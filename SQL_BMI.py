be2d7e7320de1b5969e9c534ead5706702947ce3
KFranciszek <krzysztof.zernicki@gmail.com>
2017-12-20 21:26:21 +0100

    Update SQL_BMI.py

--- SQL_BMI.py
+++ SQL_BMI.py
@@ -1,1 +1,15 @@
-#zapis wyników  do bazy sql 
+import psycopg2
+from dicBMI  import team
+
+try:
+    for bmiDic  in team:
+        conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgresq123'")
+        print("Dobre połączenie do bazy postgres")
+        cur = conn.cursor()
+        cur.execute("""INSERT INTO bmi(name,weight,height,bmi) VALUES (%(name)s,%(weight)s, %(height)s,%(bmi)s)""", bmiDic)
+        # cur.execute("INSERT INTO public.bmi (name) VALUES  ((weight))", dict[bmiDic])
+        conn.commit()
+        cur.close()
+
+except:
+    print ("I am unable to connect to the database")
