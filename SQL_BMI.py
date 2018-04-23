da38e9c58ab655984e3ca44db5f2ccf2c0a1f78a
KFranciszek <krzysztof.zernicki@gmail.com>
2018-04-23 20:25:44 +0200

    Update SQL_BMI.py

--- SQL_BMI.py
+++ SQL_BMI.py
@@ -6,7 +6,7 @@ try:
         conn = psycopg2.connect("dbname='XYZ' user='XYZ' host='localhost' password='XYZ'")
         print("Dobre połączenie do bazy postgres")
         cur = conn.cursor()
-        cur.execute("""INSERT INTO bmi(name,weight,height,bmi) VALUES (%(name)s,%(weight)s, %(height)s,%(bmi)s)""", bmiDic)
+        cur.execute("""INSERT INTO bmi(name,weight,height,bmi, gender) VALUES (%(name)s,%(weight)s, %(height)s,%(bmi)s,%(gender)s)""", bmiDic)
         conn.commit()
         cur.close()
 
