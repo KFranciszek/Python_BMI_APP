55eb86ed5c6ec42dc5aa324fad997e12bb239b9a
KFranciszek <krzysztof.zernicki@gmail.com>
2018-04-23 20:28:58 +0200

    Update score.py

--- score.py
+++ score.py
@@ -1,11 +1,25 @@
-def scoreBMI_data (bmi):
+# def scoreBMI_data (bmi):
 
-    if bmi <= 19:
+#     if bmi <= 19:
+#         return "Niedowaga"
+
+#     elif  bmi <= 25:
+#         return "Norma"
+
+#     elif bmi >= 26:
+#         return "Nadwaga"
+
+    if bmi <= 19 and  gender == 'Male':
         return "Niedowaga"
-
-    elif  bmi <= 25:
+    elif bmi <= 19 and gender == 'Famale':
         return "Norma"
-
-    elif bmi >= 26:
+    elif bmi < 18 and gender == 'Famale':
+        return "Niedowaga"
+    if bmi <= 25 and gender == 'Male':
+        return "Norma"
+    elif bmi <= 25 and gender == 'Famale':
         return "Nadwaga"
-
+    if bmi >= 26 and gender == 'Male':
+        return "Nadwaga"
+    elif bmi >= 26 and gender == 'Famale':
+        return "Otyłość"
